95e76b9cecb130427edb0d5e5ea37a5f4dd84ead
Mnilzk <mnilzk.help@gmail.com>
2025-06-01 21:14:57 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-# Install system dependencies
-apt-get update && apt-get install -y libzbar0 poppler-utils tesseract-ocr
-
-# Install Python dependencies
-pip install -r requirements.txt
